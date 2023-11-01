e4576b8381d32c464f8197326391c03b26939d84
vzaicev <aliximikmail@gmail.com>
2023-11-01 18:35:18 +0100

    [+] added example with schedule

--- odve/script/schedule/schedule.py
+++ odve/script/schedule/schedule.py
@@ -1,44 +1,125 @@
+"""Minimal jobshop example."""
+import collections
 from ortools.sat.python import cp_model
 
-# Create a CP model
-model = cp_model.CpModel()
 
-# Define the time range (adjust as needed)
-time_range = 48  # Specify the time range in hours
+def main():
+    """Minimal jobshop problem."""
+    # Data.
+    jobs_data = [  # task = (machine_id, processing_time).
+        [(0, 3), (1, 2), (2, 2)],  # Job0
+        [(0, 2), (2, 1), (1, 4)],  # Job1
+        [(1, 4), (2, 3)],  # Job2
+    ]
 
-# Define the tasks with different durations and the range of workers
-tasks = []
-task_durations = [2, 3, 1, 4, 2, 2, 3, 1, 4, 3]  # Example durations for 10 tasks
-min_workers = 3  # Minimum number of workers for a task
-max_workers = 10  # Maximum number of workers for a task
+    machines_count = 1 + max(task[0] for job in jobs_data for task in job)
+    all_machines = range(machines_count)
+    # Computes horizon dynamically as the sum of all durations.
+    horizon = sum(task[1] for job in jobs_data for task in job)
 
-# Define the tasks and add constraints
-for i, duration in enumerate(task_durations):
-    start_var = model.NewIntVar(0, time_range, f'task_{i}_start')  # Adjusted time range
-    end_var = model.NewIntVar(0, time_range, f'task_{i}_end')  # Adjusted time range
-    workers_var = model.NewIntVar(min_workers, max_workers, f'task_{i}_workers')  # Number of workers variable
-    tasks.append((start_var, end_var, workers_var))
+    # Create the model.
+    model = cp_model.CpModel()
 
-    # Add constraint: task end time is task start time + duration
-    model.Add(end_var == start_var + duration)
+    # Named tuple to store information about created variables.
+    task_type = collections.namedtuple("task_type", "start end interval")
+    # Named tuple to manipulate solution information.
+    assigned_task_type = collections.namedtuple(
+        "assigned_task_type", "start job index duration"
+    )
 
-# Add constraint: Tasks do not overlap unless running in parallel
-for i in range(len(tasks)):
-    for j in range(i + 1, len(tasks)):
-        overlap_var = model.NewBoolVar(f'overlap_{i}_{j}')
-        model.Add(overlap_var == model.NewBoolVar(f'bool_overlap_{i}_{j}'))
-        model.Add(tasks[i][1] <= tasks[j][0] + time_range * (1 - overlap_var))
-        model.Add(tasks[j][1] <= tasks[i][0] + time_range * (1 - overlap_var))
+    # Creates job intervals and add to the corresponding machine lists.
+    all_tasks = {}
+    machine_to_intervals = collections.defaultdict(list)
 
-# Define the solver
-solver = cp_model.CpSolver()
+    for job_id, job in enumerate(jobs_data):
+        for task_id, task in enumerate(job):
+            machine, duration = task
+            suffix = f"_{job_id}_{task_id}"
+            start_var = model.NewIntVar(0, horizon, "start" + suffix)
+            end_var = model.NewIntVar(0, horizon, "end" + suffix)
+            interval_var = model.NewIntervalVar(
+                start_var, duration, end_var, "interval" + suffix
+            )
+            all_tasks[job_id, task_id] = task_type(
+                start=start_var, end=end_var, interval=interval_var
+            )
+            machine_to_intervals[machine].append(interval_var)
 
-# Solve the problem
-status = solver.Solve(model)
+    # Create and add disjunctive constraints.
+    for machine in all_machines:
+        model.AddNoOverlap(machine_to_intervals[machine])
 
-if status == cp_model.OPTIMAL:
-    print("Optimal Schedule Found:")
-    for i, (start, end, workers) in enumerate(tasks):
-        print(f"Task {i + 1}: [{solver.Value(start)} - {solver.Value(end)}] hours, Workers: {solver.Value(workers)}")
-else:
-    print("No optimal solution found.")
+    # Precedences inside a job.
+    for job_id, job in enumerate(jobs_data):
+        for task_id in range(len(job) - 1):
+            model.Add(
+                all_tasks[job_id, task_id + 1].start >= all_tasks[job_id, task_id].end
+            )
+
+    # Makespan objective.
+    obj_var = model.NewIntVar(0, horizon, "makespan")
+    model.AddMaxEquality(
+        obj_var,
+        [all_tasks[job_id, len(job) - 1].end for job_id, job in enumerate(jobs_data)],
+    )
+    model.Minimize(obj_var)
+
+    # Creates the solver and solve.
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print("Solution:")
+        # Create one list of assigned tasks per machine.
+        assigned_jobs = collections.defaultdict(list)
+        for job_id, job in enumerate(jobs_data):
+            for task_id, task in enumerate(job):
+                machine = task[0]
+                assigned_jobs[machine].append(
+                    assigned_task_type(
+                        start=solver.Value(all_tasks[job_id, task_id].start),
+                        job=job_id,
+                        index=task_id,
+                        duration=task[1],
+                    )
+                )
+
+        # Create per machine output lines.
+        output = ""
+        for machine in all_machines:
+            # Sort by starting time.
+            assigned_jobs[machine].sort()
+            sol_line_tasks = "Machine " + str(machine) + ": "
+            sol_line = "           "
+
+            for assigned_task in assigned_jobs[machine]:
+                name = f"job_{assigned_task.job}_task_{assigned_task.index}"
+                # Add spaces to output to align columns.
+                sol_line_tasks += f"{name:15}"
+
+                start = assigned_task.start
+                duration = assigned_task.duration
+                sol_tmp = f"[{start},{start + duration}]"
+                # Add spaces to output to align columns.
+                sol_line += f"{sol_tmp:15}"
+
+            sol_line += "\n"
+            sol_line_tasks += "\n"
+            output += sol_line_tasks
+            output += sol_line
+
+        # Finally print the solution found.
+        print(f"Optimal Schedule Length: {solver.ObjectiveValue()}")
+        print(output)
+    else:
+        print("No solution found.")
+
+    # Statistics.
+    print("\nStatistics")
+    print(f"  - conflicts: {solver.NumConflicts()}")
+    print(f"  - branches : {solver.NumBranches()}")
+    print(f"  - wall time: {solver.WallTime()}s")
+
+
+if __name__ == "__main__":
+    main()
